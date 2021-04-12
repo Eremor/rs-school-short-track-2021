48c765539b44171b7c31f2791a1cefd0f04705d6
Eremor <artem.s13@gmail.com>
2021-04-12 09:09:15 +0300

    feat: add solution task 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let last = arr[0];
+  let count = 0;
+  let result = '';
+
+  for (let i = 0; i < arr.length + 1; i++) {
+    if (last === arr[i]) {
+      count++;
+    } else {
+      result += `${count > 1 ? count : ''}${last}`;
+      count = 1;
+      last = arr[i];
+    }
+  }
+
+  return result;
 }
 
 module.exports = encodeLine;
